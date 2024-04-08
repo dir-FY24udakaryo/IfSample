9853112bff069b0855dbaa300bfeca5c10fd83e8
RyoUdaka(宇髙凌生) <RyoUdaka()@Dirbato-1401x>
2024-04-08 12:26:50 +0900

    演習終了

--- IfSample/src/sample/IfSample.java
+++ IfSample/src/sample/IfSample.java
@@ -3,8 +3,17 @@ package sample;
 public class IfSample {
 
     public static void main(String[] args) {
-        // TODO 自動生成されたメソッド・スタブ
+        int score = 100; // 成績
 
+        if (score == 100) {
+            System.out.println("満点です！");
+        } else if (score >= 80) {
+            System.out.println("よくできました！");
+        } else if (score >= 60) {
+            System.out.println("合格です！");
+        } else {
+            System.out.println("赤点です。。。");
+        }
     }
 
 }
